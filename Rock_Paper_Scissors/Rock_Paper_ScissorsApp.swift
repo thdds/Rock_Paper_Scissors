97acd8b9f9afc7225ef9805c17c553b9374208ec
Thaddäus Schima <t.schima@mailbox.org>
2023-02-12 21:41:51 +0100

    Initial Commit

---
+++ Rock_Paper_Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock_Paper_Scissors
+//
+//  Created by Thaddäus Schima on 12.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
