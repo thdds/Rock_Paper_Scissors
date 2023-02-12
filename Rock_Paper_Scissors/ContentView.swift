5b25bf5d4b86d718a80714352476ecec40fd2f18
Thaddäus Schima <t.schima@mailbox.org>
2023-02-12 22:29:09 +0100

    built the basic interface. Not much logic tho.

--- Rock_Paper_Scissors/ContentView.swift
+++ Rock_Paper_Scissors/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var tries = 0
+    @State private var scoreTitle = ""
+    @State private var rockPaperScissors = ["✂️", "🪨", "📄"].shuffled()
+    private var correctAnswer: String {
+            return ""
+    }
+    @State private var userScore = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+            Text("\(rockPaperScissors[correctAnswer])")
+                .font(.largeTitle)
+            HStack {
+                ForEach(0..<3) {number in
+                    Button {
+                        buttonTapped(number)
+                    } label: {
+                        Text(rockPaperScissors[number]).font(.largeTitle)
+                    }
+                    
+                }
+            }
+            Text(correctAnswer)
         .padding()
+        }.alert(scoreTitle, isPresented: $showingScore) {
+            Button("Restart Game?", action: resetScore)
+        } message: {
+            Text("You got \(userScore) points.")
+        }
+        
+           
+    }
+    func buttonTapped(_ number: Int) {
+        if number == correctAnswer {
+            userScore += 1
+        } else {
+            userScore -= 1
+        }
+        tries += 1
+        printResult()
+        rockPaperScissors.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    func printResult() {
+        if tries == 10 {
+            showingScore = true
+            
+        }
+    }
+    func resetScore() {
+        tries = 0
+        userScore = 0
+    }
+    func isCorrectAnswer(_ number: Int) -> Bool {
+        return false
+    }
+    func correctAnswer(_ number: Int) -> String {
+        
+        return ""
     }
 }
 
